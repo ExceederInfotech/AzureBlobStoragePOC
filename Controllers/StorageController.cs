fc2bf1d83789720b286a43ac21ad642c2f6c30b5
Snehal <snehal.mankar@exceederinfotech.com>
2025-03-07 14:13:04 +0530

    Azure blob storage

---
+++ Controllers/StorageController.cs
@@ -0,0 +1,32 @@
+﻿using AzurBlobStorageAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AzurBlobStorageAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StorageController : ControllerBase
+    {
+        private readonly AzureBlobService _service;
+        public StorageController(AzureBlobService service)
+        {
+            _service = service;
+        }
+
+        [HttpPost]
+        [Route("UploadBlobImage")]
+        public async Task<IActionResult> UploadBlobs(IFormFile file)
+        {
+            var response = await _service.UploadFile(file);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [Route("GetAllBlobs")]
+        public async Task<IActionResult> GetAllBlobs()
+        {
+            var response = await _service.GetUploadedBlobs();
+            return Ok(response);
+        }
+    }
+}
---
+++ Services/AzureBlobService.cs
@@ -0,0 +1,56 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+
+namespace AzurBlobStorageAPI.Services
+{
+    public class AzureBlobService
+    {
+        readonly BlobServiceClient _blobClient;
+        readonly BlobContainerClient _containerClient;
+        private readonly string?  azureConnectionString = null;          
+        private readonly string containerName;
+        public AzureBlobService(IConfiguration configuration)
+        {
+            azureConnectionString = configuration.GetConnectionString("AzureStorageConnectionString");
+            _blobClient = new BlobServiceClient(azureConnectionString);
+            containerName = "blogimages";
+            _containerClient = _blobClient.GetBlobContainerClient(containerName);
+        }
+
+        public async Task<List<Azure.Response<BlobContentInfo>>> UploadFile(IFormFile file)
+        {
+            var azureResponse = new List<Azure.Response<BlobContentInfo>>();
+            string fileName = file.FileName;
+            using (var memoryStream = new MemoryStream())
+            {
+                BlobContainerClient container = _blobClient.GetBlobContainerClient(containerName);
+                await container.CreateIfNotExistsAsync();
+                file.CopyTo(memoryStream);
+                memoryStream.Position = 0;
+                var client = await _containerClient.UploadBlobAsync(fileName, memoryStream, default);
+                azureResponse.Add(client);
+            }
+            return azureResponse;
+        }
+
+        public async Task<List<BlobDto>> GetUploadedBlobs()
+        {
+            BlobContainerClient container = new (azureConnectionString, containerName);
+            List<BlobDto> files = [];
+            await foreach (BlobItem file in container.GetBlobsAsync())
+            {
+                string uri = container.Uri.ToString();
+                var name = file.Name;
+                var fullUri = $"{uri}/{name}";
+
+                files.Add(new BlobDto
+                {
+                    Uri = fullUri,
+                    Name = name,
+                    ContentType = file.Properties.ContentType
+                });
+            }
+            return files;
+        }
+    }
+}
